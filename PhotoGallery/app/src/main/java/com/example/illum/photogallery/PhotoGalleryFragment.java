fb597bd93eef330f0ea8c66712a5e9ed99b4b151
M0N3 <illuminatedzp@gmail.com>
2017-05-12 17:29:48 +0200

    Image downloading added

--- PhotoGallery/app/src/main/java/com/example/illum/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/example/illum/photogallery/PhotoGalleryFragment.java
@@ -1,8 +1,11 @@
 package com.example.illum.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -26,6 +29,7 @@ import butterknife.ButterKnife;
 
 public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = "PhotoGalleryFragment";
+    private ThumbnailDownloader<PhotoHolder> mThumbnailDownloader;
     private List<GalleryItem> mItems = new ArrayList<>();
     @BindView(R.id.fragment_photo_gallery_recycler_view)
     RecyclerView mPhotoRecyclerView;
@@ -35,10 +39,39 @@ public class PhotoGalleryFragment extends Fragment {
     }
 
     @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailDownloader.clearQueue();
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
         new FetchItemsTask().execute();
+        Handler responseHandler = new Handler();
+        mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
+        mThumbnailDownloader.setThumbnailDownloadListener(
+                new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>() {
+                    @Override
+                    public void onThumbnailDownloaded(PhotoHolder photoHolder,
+                                                      Bitmap bitmap) {
+                        Drawable drawable = new BitmapDrawable(getResources(),
+                                bitmap);
+                        photoHolder.bindDrawable(drawable);
+                    }
+                }
+        );
+        mThumbnailDownloader.start();
+        mThumbnailDownloader.getLooper();
+        Log.i(TAG, "Background thread started");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mThumbnailDownloader.quit();
+        Log.i(TAG, "Background thread destroyed");
     }
 
     @Override
@@ -108,6 +141,8 @@ public class PhotoGalleryFragment extends Fragment {
             Drawable placeholder = getResources().getDrawable
                     (R.drawable.bill_up_close);
             photoHolder.bindDrawable(placeholder);
+            mThumbnailDownloader.queueThumbnail(photoHolder,
+                    galleryItem.getUrl());
         }
 
         @Override
